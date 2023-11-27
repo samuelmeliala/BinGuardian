644f58d3bad659ee2e1dc721b39135193c34ae1e
Samuel Benediktus Meliala <brunodargo.bella@gmail.com>
2023-11-27 14:57:38 +0700

    Adding profile, billing, and home page with css

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Form submitted successfully!');
-    window.location.href = 'login.html'; // replace 'login.html' with the path to your login page
-});
